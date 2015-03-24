d1a870aa210c21ce53530e13f249f959c7a9db65
Mayank Vachher <mayankv0207@gmail.com>
2015-03-24 14:33:49 +0530

    Update ExtractTable.py
    Update Datatype conversion in reading file

--- cassis/io/kurucz/ExtractTable.py
+++ cassis/io/kurucz/ExtractTable.py
@@ -15,8 +15,8 @@ from pandas import read_fwf
 def extract_table(fileLoc):
 	tableHeaders = ['ELEM','index(J)','E(cm-1)','J','label','gLande']
 	colwidths = [(0,8),(10,13),(13,26),(27,30),(30,42),(43,48)]
-	datatypes={'ELEM': object, 'index(J)': int, 'E(cm-1)': float, 'J': float, 'label': object, 'gLande': float}
-	return read_fwf(fileLoc, skiprows=38, names=tableHeaders,colspecs=colwidths,nrows=1802-38,dtype=datatypes)
+	datatypes={0: str, 1: int, 2: float, 3: float, 4: str, 5: float}
+	return read_fwf(fileLoc, skiprows=38, names=tableHeaders,colspecs=colwidths,nrows=1802-38, converters=datatypes)
 	
 if(__name__ == "__main__"):
 	print "Call function only from MainDataset File!"
